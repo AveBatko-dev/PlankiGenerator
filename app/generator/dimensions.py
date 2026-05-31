9202a48a0eafe5af64a4e309ba94eaa78183f0dd
AveBatko-dev <uabanderakyborgua@gmail.com>
2026-05-31 15:49:31 +0300

    Зробити зворотний відступ виносних ліній видимим

--- app/generator/dimensions.py
+++ app/generator/dimensions.py
@@ -9,7 +9,7 @@ from .settings import DIMENSION_HOOK_CLEARANCE_FACTOR, get_dimension_attribs, ge
 from .types import Point, Segment
 
 DIMENSION_TEXT_GAP = 5
-DIMENSION_EXTENSION_BACKSTEP = 4
+DIMENSION_EXTENSION_BACKSTEP = 10
 
 
 def get_dimension_normal(start: Point, end: Point, side: str) -> Point:
